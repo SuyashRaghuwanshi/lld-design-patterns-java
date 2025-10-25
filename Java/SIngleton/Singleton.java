c31573295d3cc5aa2b18772bbc769ce5626d2c26
Suyash <suyashraghuwanshi322004@gmail.com>
2025-10-25 11:22:40 +0530

    singleton update

--- Java/SIngleton/Singleton.java
+++ Java/SIngleton/Singleton.java
@@ -1,4 +1,7 @@
 package Java.SIngleton;
+
+//Singleton pattern is a creational design pattern that ensures a class has only one instance
+//and provides a global point of access to that instance.
 public class Singleton{
     
     //Lazy Initialization Singleton pattern
